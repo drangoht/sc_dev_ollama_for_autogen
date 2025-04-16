c2a5592a48f70abb80a33257282a6358136683ec
Adesoji Alu <adesoji.alu@gmail.com>
2025-04-16 21:43:54 +0100

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,4 +2,4 @@ curl -fsSL https://ollama.com/install.sh | OLLAMA_VERSION=0.3.13 sh
 nohup ollama serve &
 sleep 5
 ollama pull llama3.2
-python3.12 -m pip install -r requirements.txt
+python3.11.6 -m pip install -r requirements.txt
